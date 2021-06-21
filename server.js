8a52dbbf7276a74dc7933bd3d74d5ad704fbb3d5
Dave Glad <mdaveglad@gmail.com>
2021-06-21 01:00:49 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+//bring routes
+const routeBlog = require('./routes/blog')
+
+//app
+const app = express()
+dotenv.config()
+
+//middelwares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+//routes middelwares
+app.use(process.env.API, routeBlog)
+
+//cors 
+if (process.env.NODE_ENV == 'dev') {
+    app.use(cors({origin : `${process.env.CLIENT_URL}`}))
+}
+
+mongoose.connect(process.env.DADATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(() =>{
+    console.log('Database connected')
+}).catch(err => console.log(err))
+
+
+let port = process.env.PORT || 8081
+
+app.listen(port, () => console.log(`server is run ${port}`))
